868e6b256dc012d60f48081a5102e172bd3f773e
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-05-09 03:20:25 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,9 +11,9 @@ def loop_iterator(number_of_times)
   loop do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     number_of_times += 1
-    break if number_of_times == 7
+    if number_of_times < 7
+    break
   end
-  
 end
 
 def times_iterator(number_of_times)
